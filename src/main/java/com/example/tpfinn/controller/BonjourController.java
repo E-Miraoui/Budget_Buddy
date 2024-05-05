fb4528c849387633d8f5515a814e518dab983338
mo-hamedaziz <mohamedaziz.bchini@insat.ucar.tn>
2024-05-05 10:42:17 +0100

    [+] Adding BonjourController, for real this time (DPA)

--- src/main/java/com/example/tpfinn/controller/BonjourController.java
+++ src/main/java/com/example/tpfinn/controller/BonjourController.java
@@ -1,2 +1,15 @@
-package com.example.tpfinn.controller;public class BonjourController {
+package com.example.tpfinn.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+
+public class BonjourController {
+    @GetMapping("/")
+    public String sayBonjour() {
+        return "Bonjour !";
+    }
 }
