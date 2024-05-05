698f9cbe51ee495c1740989024329a4dba9849c0
mo-hamedaziz <mohamedaziz.bchini@insat.ucar.tn>
2024-05-05 21:05:01 +0100

    Adding react app

--- src/main/java/com/example/tpfinn/repository/ItemCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.tpfinn.repository;
-
-import com.example.tpfinn.model.ItemCategoryModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ItemCategoryRepository extends MongoRepository<ItemCategoryModel, String> {
-}
--- src/main/java/com/example/tpfinn/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.tpfinn.repository;
-
-import com.example.tpfinn.model.UserModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<UserModel, String> {
-
-}
