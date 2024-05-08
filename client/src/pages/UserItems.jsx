8cb2ee1cd62e4e94968f2216809732e5bea942f7
mo-hamedaziz <mohamedaziz.bchini@insat.ucar.tn>
2024-05-08 17:08:06 +0100

    Releasing v1.1

--- client/src/pages/UserItems.jsx
+++ client/src/pages/UserItems.jsx
@@ -1,29 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation, useParams } from 'react-router-dom'; // Import useLocation hook
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import FontAwesomeIcon
-import Item from '../components/itemcomponent/item';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from "react";
+import { useLocation, useParams } from "react-router-dom"; // Import useLocation hook
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import FontAwesomeIcon
+import Item from "../components/itemcomponent/item";
+import "bootstrap/dist/css/bootstrap.min.css";
 //import backgroundImage from '../../assets/background.jpg'; // Import the background image
 
 function Dashboard() {
   const [user, setUser] = useState(null);
   const [items, setItems] = useState([]);
-  const { userId } = useParams(); 
-  const location = useLocation(); 
+  const { userId } = useParams();
+  const location = useLocation();
 
   useEffect(() => {
     async function fetchUserData() {
       try {
         const headers = {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         };
 
         // Fetch items based on the user ID
-        const itemsResponse = await fetch(`http://localhost:8080/api/v1/user/${userId}/items`, { headers });
+        const itemsResponse = await fetch(
+          `http://localhost:8080/api/v1/user/${userId}/items`,
+          { headers }
+        );
         const itemsData = await itemsResponse.json();
         setItems(itemsData);
       } catch (error) {
-        console.error('Error fetching user data:', error);
+        console.error("Error fetching user data:", error);
       }
     }
     fetchUserData();
@@ -32,21 +35,24 @@ function Dashboard() {
   return (
     <div className="container-fluid">
       {items && (
-        <div className="row">
-          <main className="col-lg-9 col-sm-12">
-            <div className="row row-cols-3">
-              {
-                items.length > 0 ? (
+        <>
+          <div className="item-counter">
+            You bought {items.length} items this month
+          </div>
+          <div className="row">
+            <main className="col-lg-9 col-sm-12">
+              <div className="row row-cols-3">
+                {items.length > 0 ? (
                   items.map((i, id) => {
-                    return (<Item key={id} item={i} />)
+                    return <Item key={id} item={i} />;
                   })
                 ) : (
                   <h1> </h1>
-                )
-              }
-            </div>
-          </main>
-        </div>
+                )}
+              </div>
+            </main>
+          </div>
+        </>
       )}
     </div>
   );
